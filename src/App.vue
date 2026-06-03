552b4d513b8f171451a47f6a7a2b840c0542a3d0
moazali06 <moaz55397@gmail.com>
2026-06-03 09:48:44 +0500

    Moaz Push Frontend code

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+<TheNavbar />
+  <HeroSection />
+  <Footer />
+  
+  </template>
+
+<script setup>
+// HeroSection component ko import kar rahe hain
+import HeroSection from './components/HeroSection.vue'
+import TheNavbar from './components/Navbar.vue'
+import Footer from './components/Footer.vue'
+
+</script>
+
+<style>
+/* Global styles agar chahiye hon, warna Tailwind handle kar lega */
+body {
+  margin: 0;
+  padding: 0;
+  background-color: #0A192F; /* Dark background default */
+}
+</style>
